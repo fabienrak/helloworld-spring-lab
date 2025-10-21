c46fa32f4fb9bd2d9fb8887514043af37468787e
fabienrak <mdgredteam@gmail.com>
2025-10-21 17:37:11 +0300

    chore: calcul

--- src/main/java/org/app/controllers/IndexController.java
+++ src/main/java/org/app/controllers/IndexController.java
@@ -1,7 +1,9 @@
 package org.app.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +13,11 @@ public class IndexController {
     @GetMapping("hello")
     public String sayHelloString(){
         return "################## hELLo WORLLD #########################";
+    }
+
+    @GetMapping("addition/{x}/{y}")
+    public Integer addNumber(@PathVariable Integer x, @PathVariable Integer y){
+
+        return x + y;
     }
 }
